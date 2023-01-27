4a3a063c3f283050c4984c5dc870bb5b1a08398d
Rushil Singh <rusingh@synamedia.com>
2023-01-27 13:27:20 +0530

    Create separate component for entries

--- src/components/logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-function Logo(logo, width = "auto", height = "auto") {
-  return (<p>
-    <img class="logo" src={logo} width={width} height={height}  />
-  </p>)
-
-}
-  
-export default Logo
