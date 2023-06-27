3947f130150c46cca1e750ab74038568ab7ce5be
Rushil Singh <rusingh@synamedia.com>
2023-06-27 14:33:56 +0530

    Update bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,6 +1,9 @@
 import React from "react"
 export default function Bio() {
-  return <><br /><br /><p style={{ color: "white" }}><b></b>
-With over six years of experience, I am a proficient software engineer specializing in Python-driven AWS data engineering pipelines, full-stack application development utilizing JavaScript frameworks, and cross-platform app creation with Flutter. My expertise in serverless architectures, adherence to clean coding principles, and commitment to lifelong learning have consistently resulted in the successful delivery of top-notch projects.  </p><br /></>
-
+  return <>
+    <br /><br />
+    <p style={{ color: "white" }}><b></b>
+      With over six years of experience, I am a proficient software engineer specializing in data engineering pipelines, full-stack application development, and cross-platform app development.
+    </p>
+    <br /></>
 }
